04e53742f2265be3d7ebcb9138d8907752927ad5
Edvards <edvardskrumins@gmail.com>
2026-05-27 22:28:27 +0300

    conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,9 +1,13 @@
 return {
     "stevearc/conform.nvim",
     config = function()
+        local gopath = vim.fn.system("go env GOPATH"):gsub("%s+", "")
+        vim.env.PATH = vim.env.PATH .. ":" .. gopath .. "/bin"
+
         require("conform").setup({
             formatters_by_ft = {
                 php = { "pint" },
+                go = { "goimports" },
             },
             formatters = {
                 pint = {
